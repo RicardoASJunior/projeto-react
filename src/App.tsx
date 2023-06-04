deec7cffbe2eb9c1fc880aaf2f3276cc34bd671f
Ricardo <ric.asjunior@gmail.com>
2023-06-04 18:03:06 -0300

    Criando Elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import React from 'react'
+
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Catalogo from './components/pages/Catalogo';
+import Novo from './components/pages/Novo'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <p>Funciona PFV</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/Novo" element={<Novo />}>
+        <Route index element={<Catalogo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
